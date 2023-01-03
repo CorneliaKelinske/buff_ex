289141e0fa45482214744823f94c074fe95edff8
cornelia <corneliakelinske@gmail.com>
2023-01-03 06:59:02 -0800

    add test

---
+++ test/buff_ex/canadian_protein/casein_test.exs
@@ -0,0 +1,17 @@
+defmodule BuffEx.CanadianProtein.CaseinTest do
+  use ExUnit.Case, async: true
+  alias BuffEx.CanadianProtein.Casein
+
+  @moduletag :buff_ex_external
+
+  describe "@find/1" do
+
+    test "returns a tuple with :ok and a Casein struct when the HTTP request returns a valid document" do
+      assert {:ok, %Casein{}} = Casein.find()
+    end
+
+    test "returns error tuple when HTTP request does not return a valid document" do
+      assert {:error, _error} = Casein.find("https://canadianprotein.com/product")
+    end
+  end
+end
