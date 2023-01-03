745179d3077a8e12c77d590eeb2ea16325e6f6d7
cornelia <corneliakelinske@gmail.com>
2023-01-03 07:14:18 -0800

    fix typespecs

--- test/buff_ex/canadian_protein/casein_test.exs
+++ test/buff_ex/canadian_protein/casein_test.exs
@@ -5,7 +5,6 @@ defmodule BuffEx.CanadianProtein.CaseinTest do
   @moduletag :buff_ex_external
 
   describe "@find/1" do
-
     test "returns a tuple with :ok and a Casein struct when the HTTP request returns a valid document" do
       assert {:ok, %Casein{}} = Casein.find()
     end
